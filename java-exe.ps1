32fab79ba8ee283198631548b4eb46bc2595cfd3
tuNovio <alfgomachi@gmail.com>
2023-11-13 22:13:55 -0500

    refactor: clases is the list of directories wich contains the java clases

--- java-exe.ps1
+++ java-exe.ps1
@@ -1,5 +1,5 @@
 $clases = " ./src/*.java ./src/Interfaz/*.java ./src/Mundo/*.java ./src/Mundo/Modelos/*.java ./src/Utils/*.java ./src/Visual/*.java"
-$compile = "javac -d ./bin/ " + "$class"
+$compile = "javac -d ./bin/ " + "$clases"
 $CreateJarFile = "jar -cfm Joogle.jar Manifesto.txt -C ./bin/ ."
 $JavaCommand = "java -jar Joogle.jar -f .\src\Joogle.java "
 $RunCommand = "$compile" + " && " + "$CreateJarFile" + " && " + "$JavaCommand"
