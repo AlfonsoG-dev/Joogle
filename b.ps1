f088437637ba9b63fe95cc6538bd1006e6cb83b9
AlfonsoG-dev <alfgomachi@gmail.com>
2024-05-19 00:19:21 -0500

    chore: removed test build ps1 file

--- b.ps1
+++
@@ -1,4 +0,0 @@
-$buildCommand = "javabuild --build"
-$runCommand = "java -jar Joogle.jar -f .\src\testing\Pruebas.java 'Pruebas()'"
-$command = "$buildCommand" + " && " + "$runCommand"
-Invoke-Expression $command
