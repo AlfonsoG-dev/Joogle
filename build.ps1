e327e805f67411c370b69b513c2aa2d5a1ef6303
AlfonsoG-dev <alfgomachi@gmail.com>
2025-06-23 18:33:06 -0500

    build: update build script

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
-$srcClases = ".\src\*.java .\src\Interfaz\*.java .\src\Mundo\*.java .\src\Mundo\Modelos\*.java .\src\Utils\*.java .\src\Visual\*.java "
+$srcClases = "src\*.java src\Interfaz\*.java src\Mundo\*.java src\Mundo\Modelos\*.java src\Utils\*.java src\Visual\*.java "
 $libFiles = ""
-$compile = "javac -Werror -Xlint:all -d .\target\ $srcClases"
-$createJar = "jar -cfe Joogle.jar Joogle -C .\bin\ ."
+$compile = "javac --release 23 -Werror -Xlint:all -d .\bin\ $srcClases"
+$createJar = "jar -cfm Joogle.jar Manifesto.txt -C .\bin\ ."
 $javaCommand = "java -jar Joogle.jar"
 $runCommand = "$compile" + " && " + "$createJar" + " && " +"$javaCommand"
 Invoke-Expression $runCommand 
